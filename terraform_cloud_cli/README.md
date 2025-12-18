9ac7fa64464647e6300823561b67c185f33d8b4d
jcaessaroTwo <jcaessaro2@gmail.com>
2025-12-18 10:59:06 +0100

    Delete terraform_cloud_cli directory

--- terraform_cloud_cli/README.md
+++
@@ -1,3 +0,0 @@
-"# tcloud" 
-"# terraform_cloud_cli" 
-"# terraform_cloud_cli" 
--- terraform_cloud_cli/outputs.tf
+++
@@ -1,6 +0,0 @@
-# //////////////////////////////
-# OUTPUT
-# //////////////////////////////
-output "instance-ip" {
-  value = module.ec2_cluster.public_ip
-}
