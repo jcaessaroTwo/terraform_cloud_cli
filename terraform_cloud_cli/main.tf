2a8fb5270a6bf24a7e3590e4f4870e007fff2fbd
jcaessaroTwo <jcaessaro2@gmail.com>
2025-12-18 11:03:27 +0100

    Add files via upload

---
+++ terraform_cloud_cli/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.26.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "josecitoCompany"
+
+    workspaces {
+      name = "terragit"
+    }
+  }
+
+}
+
+provider "aws" {
+  region  = "eu-south-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0682c282a19c84de9"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "josecitoejemplo"
+  }
+}
